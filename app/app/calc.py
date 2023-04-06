5edf6f3e114a18007576b36daa201f2818344a34
Vinnicyus Gracindo <vini.gracindo@gmail.com>
2023-04-05 21:39:42 -0300

    Configured django

---
+++ app/app/calc.py
@@ -0,0 +1,13 @@
+"""
+Calculator library containing basic math operations.
+"""
+
+
+def add(x: float, y: float) -> float:
+    """Add x and y and return value"""
+    return x + y
+
+
+def subtract(x: float, y: float) -> float:
+    """Subtract x from y and return value"""
+    return x - y
